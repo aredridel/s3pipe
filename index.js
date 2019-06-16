11a7036bcd861a270d9eaecb160c73901978f6d6
Aria Stewart <aredridel@dinhe.net>
2019-06-16 15:26:14 -0400

    Use a partSize that will avoid exhausting RAM but still allow uploading 600gb

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ if (!process.argv[2] || !process.argv[3]) {
 
 main(async () => {
     const s3 = new AWS.S3();
-    const upload = s3.upload({ Bucket: process.argv[2], Key: process.argv[3], Body: process.stdin }, {partSize: 5 * 1024 * 1024 * 1024});
+    const upload = s3.upload({ Bucket: process.argv[2], Key: process.argv[3], Body: process.stdin }, {partSize: 60 * 1024 * 1024});
     upload.on('httpUploadProgress', console.log)
     await upload.promise();
 });
